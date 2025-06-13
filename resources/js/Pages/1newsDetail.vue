e723b6b97ebafeeeb07f010868603b7bd0868b62
0nvendra <onvendra@gmail.com>
2025-06-13 14:32:20 +0700

    2.1.3a fix bisa video youtube di content news

--- resources/js/Pages/1newsDetail.vue
+++ resources/js/Pages/1newsDetail.vue
@@ -2,12 +2,30 @@
 import { Head, Link } from '@inertiajs/vue3';
 import { VuePreloader } from 'vue-preloader';
 import '/node_modules/vue-preloader/dist/style.css';
-const props = defineProps({ blog: Object })
+
+const props = defineProps({
+    blog: Object
+});
+
+// Fungsi helper untuk konversi link YouTube ke embed iframe
+function convertYoutubeLinksToIframe(content) {
+    const youtubeRegex = /https?:\/\/(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)/g;
+
+    return content.replace(youtubeRegex, (match, videoId) => {
+        return `
+      <div class="ratio ratio-16x9 my-3">
+        <iframe src="https://www.youtube.com/embed/${videoId}" 
+                title="YouTube video" 
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+                allowfullscreen></iframe>
+      </div>`;
+    });
+}
 </script>
 
 <template>
 
-    <Head title="News Title" />
+    <Head :title="blog.title" />
 
     <section id="services" class="services section-bg mb-10" style="padding-top: 10%;">
         <div class="container mt-5" data-aos="fade-down">
@@ -20,6 +38,7 @@ const props = defineProps({ blog: Object })
                     <img class="card-img-top"
                         :src="blog.image ? `/storage/${blog.image}` : '/assets/img/projects/lotte-catalyst/catalyst (4).jpeg'"
                         alt="Card image cap" style="max-height: 30vh; object-fit: cover; width: 100%;" />
+
                     <div class="d-flex justify-content-between align-items-center my-3 text-muted"
                         style="font-size: 0.95em;">
                         <div>
@@ -29,12 +48,15 @@ const props = defineProps({ blog: Object })
                         <div>
                             <i class="bi bi-calendar-event"></i>
                             {{ new Date(blog.published_at).toLocaleDateString('id-ID', {
-                                year: 'numeric', month: 'long',
+                                year: 'numeric',
+                                month: 'long',
                                 day: 'numeric'
                             }) }}
                         </div>
                     </div>
-                    <div v-html="blog.content"></div>
+
+                    <!-- Render konten dengan auto embed YouTube -->
+                    <div v-html="convertYoutubeLinksToIframe(blog.content)"></div>
                 </div>
             </div>
         </div>
